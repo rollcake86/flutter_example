095d4f3648827fb209005660b5dced2c3f2cadf4
chojunsu <ikinye86@gmail.com>
2019-07-02 13:27:37 +0900

    example app update

---
+++ tap_example/tab_example/lib/tab.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:tab_example/tab/first.dart';
+import 'package:tab_example/tab/second.dart';
+import 'package:tab_example/tab/third.dart';
+
+
+class TabHome extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return new TabHomeApp();
+  }
+}
+
+class TabHomeApp extends State<TabHome> with SingleTickerProviderStateMixin {
+
+  TabController controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Initialize the Tab Controller
+    controller = new TabController(length: 3, vsync: this);
+  }
+
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  TabBar getTabBar() {
+    return new TabBar(tabs: <Tab>[
+      new Tab(icon: new Icon(Icons.favorite),),
+      new Tab(icon: new Icon(Icons.adb),),
+      new Tab(icon: new Icon(Icons.airport_shuttle),),
+    ],
+      controller: controller,
+    );
+  }
+
+  TabBarView getTabBarView(var tabs) {
+    return new TabBarView(
+      // Add tabs as widgets
+      children: tabs,
+      // set the controller
+      controller: controller,
+    );
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      // Appbar
+        appBar: new AppBar(
+          // Title
+            title: new Text("Using Tabs"),
+            // Set the background color of the App Bar
+            backgroundColor: Colors.blue,
+            // Set the bottom property of the Appbar to include a Tab Bar
+            bottom: getTabBar()),
+        // Set the TabBar view as the body of the Scaffold
+        body: getTabBarView(<Widget>[new First(), new Second(), new Third()]));
+  }
+}
+
